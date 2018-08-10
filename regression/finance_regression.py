795f2d62e068d3865ae8790f439bf145e68053cd
Tomas + Luca <tomasbasham+pairing@gmail.com>
2018-08-10 12:32:40 +0000

    Implemented Regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -2,7 +2,7 @@
 
 """
     Starter code for the regression mini-project.
-    
+
     Loads up/formats a modified version of the dataset
     (why modified?  we've removed some trouble points
     that you'll find yourself in the outliers mini-project).
@@ -10,8 +10,7 @@
     Draws a little scatterplot of the training/testing data
 
     You fill in the regression code where indicated:
-"""    
-
+"""
 
 import sys
 import pickle
@@ -19,7 +18,7 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
 
-### list the features you want to look at--first item in the 
+### list the features you want to look at--first item in the
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
@@ -29,41 +28,59 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
-
-
+test_color = "r"
 
 ### Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
+### Please name it reg, so that the plotting code below picks it up and
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from time import time
+from sklearn import linear_model
 
+reg = linear_model.LinearRegression()
 
+t0 = time()
+reg.fit(feature_train, target_train)
+print "training time:", round(time()-t0, 3), "s"
 
+print "regression coefficent:", reg.coef_
+print "regression intercept:", reg.intercept_
+print
 
+t0 = time()
+score = reg.score(feature_test, target_test)
+print "testing time:", round(time()-t0, 3), "s"
 
+print "score:", score
+print
 
+### Swap around the train and test data
+reg2 = linear_model.LinearRegression()
+reg2.fit(feature_test, target_test)
+
+print "regression 2 coefficent:", reg2.coef_
+print "regression 2 intercept:", reg2.intercept_
+print
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
 for feature, target in zip(feature_test, target_test):
-    plt.scatter( feature, target, color=test_color ) 
+    plt.scatter( feature, target, color=test_color )
 for feature, target in zip(feature_train, target_train):
-    plt.scatter( feature, target, color=train_color ) 
+    plt.scatter( feature, target, color=train_color )
 
 ### labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
-
-
-
 ### draw the regression line, once it's coded
 try:
-    plt.plot( feature_test, reg.predict(feature_test) )
+    plt.plot(feature_test, reg.predict(feature_test))
+    plt.plot(feature_train, reg2.predict(feature_train), color="b")
 except NameError:
     pass
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
