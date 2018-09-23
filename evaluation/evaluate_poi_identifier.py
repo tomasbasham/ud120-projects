41f88b2768bf81c748a8206146fc51ca24f27e38
Tomas Basham <tomasbasham@gmail.com>
2018-09-23 20:05:54 +0000

    Explore different evaluation metrics
    Accuracy is defined as the quotient of the number of data points
    labelled correctly and the total number of data points. However this is
    not ideal for skewed classes.
    
    Precision is the quotient of the number of data points labelled
    correctly (true positives) and the total number of true positives plus
    false positives. Or simply, of all the data points labelled as one class
    how many truly belong to the that class.
    
    Recall is the quotient of the number of data points labelled correctly
    (true positives) and the total number of true positives plus false
    negatives. Or simply, of all the data points belonging as one class, how
    many were correctly classified.
    
    F1 score is a weighted average of the precision and recall, where an F1
    score reaches its best value at 1 and worst value at 0. The relative
    contribution of precision and recall to the F1 score are equal.

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 """
     Starter code for the evaluation mini-project.
     Start by copying your trained/tested POI identifier from
@@ -15,6 +14,7 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from time import time
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -24,8 +24,35 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+### your code goes here
 
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
-### your code goes here 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
+clf = DecisionTreeClassifier()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+t0 = time()
+accuracy = accuracy_score(labels_test, pred)
+precision = precision_score(labels_test, pred)
+recall = recall_score(labels_test, pred)
+print "testing time:", round(time()-t0, 3), "s"
+
+print "accuracy:", accuracy
+print "precision:", precision
+print "recall:", recall
+print
+
+print "number of people in test set:", len(pred)
+print "number of predicted persons of interest:", sum(pred)
+print "number of true positives:", len([i for i in zip(labels_test,[float(i) for i in pred]) if i[0] * i[1] == 1])
